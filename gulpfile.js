b44d891b73fb154b6c8031f3658e2c65aa50873a
Ray Villalobos <ray@planetoftheweb.com>
2014-02-14 14:28:46 -0500

    basic gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,11 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    livereload = require('gulp-livereload'),
-    lr = require('tiny-lr'),
-    server = lr();
-
-var jsSources = [
-  'components/lib/jquery/jquery.js',
-  'components/scripts/*.js'
-];
+  gutil = require('gulp-util'),
+  uglify = require('gulp-uglify'),
+  concat = require('gulp-concat');
 
 gulp.task('js', function() {
-  gulp.src(jsSources)
+  gulp.src('components/scripts/*.js')
     .pipe(uglify())
     .pipe(concat('script.js'))
     .pipe(gulp.dest('js'));
 });
-
-gulp.task('watch', function() {
-  var server = livereload();
-  gulp.watch('*.html', function(e) {
-    server.changed(e.path);
-  });
-  gulp.watch(jsSources,['js']);
-})
-
-gulp.task('default', ['js', 'watch']);
