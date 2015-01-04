bbcce1ac6e9056cb5c2e96bb43b5b24f5d32ca3e
Ray Villalobos <ray@planetoftheweb.com>
2015-01-03 20:52:07 -0500

    02_05 Working with conditional statements

--- components/coffee/script.coffee
+++ components/coffee/script.coffee
@@ -1,14 +1,2 @@
-a = [10..1]
-
-names = [
-  'Barot Bellingham'
-  'Constance Smith'
-  'Hassum Harrod'
-  'Hillary Goldwynn'
-  'Xhou Ta'
-  ]
-
-names[1..2] = ['Bob', 'Pete']
-
-for item in names
-  console.log(item)
+name = 'Ray'
+$('#content').append "#{name}" if name?
--- components/scripts/script.js
+++ components/scripts/script.js
@@ -1,12 +1,7 @@
-var a, item, names, _i, _len, _ref;
+var name;
 
-a = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+name = 'Ray';
 
-names = ['Barot Bellingham', 'Constance Smith', 'Hassum Harrod', 'Hillary Goldwynn', 'Xhou Ta'];
-
-[].splice.apply(names, [1, 2].concat(_ref = ['Bob', 'Pete'])), _ref;
-
-for (_i = 0, _len = names.length; _i < _len; _i++) {
-  item = names[_i];
-  console.log(item);
+if (name != null) {
+  $('#content').append("" + name);
 }
