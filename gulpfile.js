4216ff9b2837dbe5d32c64287f8a94494c730bd4
Ray Villalobos <ray@planetoftheweb.com>
2015-01-03 19:55:27 -0500

    02_06 Adding a default task and live reloading

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,33 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     uglify = require('gulp-uglify'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    livereload = require('gulp-livereload');
+
+var jsSources = [
+    'components/scripts/scriptOne.js',
+    'components/scripts/scriptTwo.js'
+];
 
 gulp.task('js', function() {
-  gulp.src(['components/scripts/scriptOne.js',
-            'components/scripts/scriptTwo.js'])
-          .pipe(uglify())
-          .pipe(concat('script.js'))
-          .pipe(gulp.dest('js'));
+  gulp.src(jsSources)
+    .pipe(uglify())
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('js'))
+    .pipe(livereload());
 });
+
+gulp.task('html', function() {
+  gulp.src('*.html')
+    .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch(jsSources, ['js']);
+  gulp.watch('*.html', ['html']);
+  gulp.watch(['js/script.js', '*.html']);
+  gulp.watch(['*.html', '*.html']);
+  livereload.listen();
+});
+
+gulp.task('default', ['js', 'html', 'watch']);
