bad45c09a0d0da1290b3436014778560f358b875
Ray Villalobos <ray@planetoftheweb.com>
2015-01-03 21:09:41 -0500

    02_08 Working with splats

--- components/coffee/script.coffee
+++ components/coffee/script.coffee
@@ -1,2 +1,13 @@
-add = (a, b) ->
-  a + b
+info = [
+  'Ray Villalobos'
+  'http://planetoftheweb.com'
+  'http://authoredcontent.com'
+  'http://iviewsource.com'
+]
+
+fillOut = (name, links...) ->
+  $('#content').append "#{name}<br>"
+  for item in links
+    $('#content').append "#{item}<br>"
+
+fillOut info...
--- components/scripts/script.js
+++ components/scripts/script.js
@@ -1,5 +1,18 @@
-var add;
+var fillOut, info,
+  __slice = [].slice;
 
-add = function(a, b) {
-  return a + b;
+info = ['Ray Villalobos', 'http://planetoftheweb.com', 'http://authoredcontent.com', 'http://iviewsource.com'];
+
+fillOut = function() {
+  var item, links, name, _i, _len, _results;
+  name = arguments[0], links = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
+  $('#content').append("" + name + "<br>");
+  _results = [];
+  for (_i = 0, _len = links.length; _i < _len; _i++) {
+    item = links[_i];
+    _results.push($('#content').append("" + item + "<br>"));
+  }
+  return _results;
 };
+
+fillOut.apply(null, info);
