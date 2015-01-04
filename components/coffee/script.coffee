6b723c3aec2b007084eea31dd309d4ecf068c99f
Ray Villalobos <ray@planetoftheweb.com>
2015-01-03 20:36:32 -0500

    02_01 Basic CoffeeScript syntax

--- components/coffee/script.coffee
+++ components/coffee/script.coffee
@@ -1,2 +1,14 @@
-$ ->
-  $('h1').append('Hello')
+data =
+  name : "Ray Villalobos"
+  numbers : [1,2,3,4]
+  links:
+    twitter: "http://twitter.com/planetoftheweb"
+    podcast: "http://authoredcontent.com"
+
+###
+block comments
+###
+
+`function add(a,b) {
+  return a+b;
+}`
--- components/scripts/script.js
+++ components/scripts/script.js
@@ -1,3 +1,19 @@
-$(function() {
-  return $('h1').append('Hello');
-});
+var data;
+
+data = {
+  name: "Ray Villalobos",
+  numbers: [1, 2, 3, 4],
+  links: {
+    twitter: "http://twitter.com/planetoftheweb",
+    podcast: "http://authoredcontent.com"
+  }
+};
+
+
+/*
+block comments
+ */
+
+function add(a,b) {
+  return a+b;
+};
